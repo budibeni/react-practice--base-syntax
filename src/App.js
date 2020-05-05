398140e48038965134d09217c7e459730eac2e71
Budi Beni <budibeni.dev@gmail.com>
2020-05-05 11:31:00 +0700

    time to practice

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
+
+class App extends Component {
+  state={
+    username:'Budi Beni'
+  }
+
+  usernameChangedHandler=(event)=>{
+    this.setState({username:event.target.value});
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <UserInput 
+          changed={this.usernameChangedHandler} 
+          currentName={this.state.username} /> 
+        <UserOutput
+          userName={this.state.username} />
+        <UserOutput
+          userName={this.state.username} />
+        <UserOutput
+          userName='Beni' />
+      </div>
+    )
+  };
 }
 
 export default App;
